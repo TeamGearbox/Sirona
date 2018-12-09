583b6a5ad192a4a8bfc1057cbf705428ce55bad8
gsessums <geoffreysessums@gmail.com>
2018-12-09 08:46:20 -0600

    Updated UserDB.java

--- src/java/sirona/data/UserDB.java
+++ src/java/sirona/data/UserDB.java
@@ -1,9 +1,16 @@
 package sirona.data;
 
+import sirona.model.User;
+
 /**
  *
  * @author Geoffrey
  */
-public class UserDb {
+public class UserDB {
+
+    public static long insert(User user) {
+        // TODO: Add code that adds the user to the database
+        return 0;
+    }
     
 }
